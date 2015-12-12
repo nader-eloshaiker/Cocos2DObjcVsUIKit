bf1023213d2672d0a94b38132891a4352d51afab
Nader Eloshaiker <nader.eloshaiker@gmail.com>
2015-12-12 13:50:23 +1100

    Initial commit for Odecee Mobile Sync.

---
+++ Cocos2DObjcVsUIKit/Classes/AppDelegate.h
@@ -0,0 +1,55 @@
+//
+//  AppDelegate.h
+//
+//  Created by : Nader Eloshaiker
+//  Project    : Cocos2DObjcVsUIKit
+//  Date       : 12/12/2015
+//
+//  Copyright (c) 2015 Nader Eloshaiker.
+//  All rights reserved.
+//
+// -----------------------------------------------------------------
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+// -----------------------------------------------------------------------
+
+@interface AppDelegate : CCAppDelegate
+
+// -----------------------------------------------------------------------
+
+@end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Cocos2DObjcVsUIKit/Classes/HelloWorldScene.h
@@ -0,0 +1,61 @@
+//
+//  HelloWorldScene.h
+//
+//  Created by : Nader Eloshaiker
+//  Project    : Cocos2DObjcVsUIKit
+//  Date       : 12/12/2015
+//
+//  Copyright (c) 2015 Nader Eloshaiker.
+//  All rights reserved.
+//
+// -----------------------------------------------------------------
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+#import "cocos2d-ui.h"
+
+// -----------------------------------------------------------------------
+
+@interface HelloWorldScene : CCScene
+
+// -----------------------------------------------------------------------
+
+- (instancetype)init;
+
+// -----------------------------------------------------------------------
+
+@end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
